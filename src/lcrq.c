b22f9b2a6069536bee76ebc6de2b45f54f6d355e
Brett A C Sheffield <bacs@librecast.net>
2022-06-02 15:21:06 +0200

    isprime() - don't check even factors
    - we've already checked for mod(2), so skip evens

--- src/lcrq.c
+++ src/lcrq.c
@@ -14,7 +14,7 @@ int isprime(int n)
 {
 	if (n <= 1) return 0;
 	if (n % 2 == 0) return 0;
-	for (int i = 3; i < n; i++) {
+	for (int i = 3; i < n; i += 2) {
 		if (n % i == 0) return 0;
 	}
 	return 1;
