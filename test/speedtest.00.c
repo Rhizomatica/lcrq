41435686dea0e839e985deeb5428699de7aeffa8
Brett A C Sheffield <bacs@librecast.net>
2022-07-10 13:20:47 +0200

    remove speedtest dependency on libsodium

--- test/speedtest.00.c
+++ test/speedtest.00.c
@@ -1,12 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only */
 /* Copyright (c) 2022 Brett Sheffield <bacs@librecast.net> */
 
+#include "test.h"
 #include <arpa/inet.h>
 #include <assert.h>
 #include <gf256.h>
 #include <lcrq.h>
 #include <lcrq_pvt.h>
-#include <sodium.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/param.h>
 #include <time.h>
@@ -30,7 +32,7 @@ uint8_t *generate_source_object(size_t F)
 {
 	uint8_t *obj = malloc(F);
 	assert(obj);
-	randombytes_buf(obj, F);
+	test_randombytes(obj, F);
 	return obj;
 }
 
